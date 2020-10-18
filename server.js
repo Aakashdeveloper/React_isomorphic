e51ad4aa70488ff16e87742940f4d5569ff6b55e
Aakashdeveloper <ahanda205@gmail.com>
2020-10-18 13:08:12 +0530

    addedfinalcode

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = process.env.PORT||9800;
+const http = require('http');
 
 app.use(express.static(path.join(__dirname,'build')));
 
@@ -9,4 +10,6 @@ app.get('/',(req,res) => {
     res.sendFile(path.join(__dirname,'build/index.html'))
 });
 
-app.listen(port);
+app.set('port',port);
+const server = http.createServer(app);
+server.listen(port);
